3887ac2f3d0837799172c5158ee1eca0d45eeceb
alin.lin <s290510790@gmail.com>
2025-05-22 22:41:03 +0800

    第五次異動修改時區

--- src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
+++ src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
@@ -6,4 +6,6 @@ import com.kujudy.springbootmall.model.Product;
 public interface ProductDao {
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
