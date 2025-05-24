2086e84887be4fea697462128ae6d7db9900293a
alin.lin <s290510790@gmail.com>
2025-05-24 21:50:03 +0800

    商品功能 分頁

--- src/main/java/com/kujudy/springbootmall/controller/ProductControll.java
+++ src/main/java/com/kujudy/springbootmall/controller/ProductControll.java
@@ -6,16 +6,19 @@ import com.kujudy.springbootmall.dto.ProductRequest;
 import com.kujudy.springbootmall.model.Product;
 import com.kujudy.springbootmall.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Locale;
 
-
+@Validated
 @RestController
 public class ProductControll {
 
@@ -28,13 +31,18 @@ public class ProductControll {
             @RequestParam(required = false) String search,
             //排序 sort
             @RequestParam(defaultValue = "create_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+            //分頁pagination 幾筆 跳過
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
             ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
         List<Product> productList= productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
