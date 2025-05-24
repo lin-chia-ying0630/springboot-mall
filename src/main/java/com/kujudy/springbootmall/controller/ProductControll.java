6e3e9ba2949ffd6ae142a6ecb57761a8b38b36bb
alin.lin <s290510790@gmail.com>
2025-05-24 20:58:12 +0800

    商品功能 desc orderBy

--- src/main/java/com/kujudy/springbootmall/controller/ProductControll.java
+++ src/main/java/com/kujudy/springbootmall/controller/ProductControll.java
@@ -23,12 +23,18 @@ public class ProductControll {
     private ProductService productService;
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
-            @RequestParam (required = false) ProductCategory  category,
-            @RequestParam (required = false) String search
+            //查詢條件filiter
+            @RequestParam(required = false) ProductCategory  category,
+            @RequestParam(required = false) String search,
+            //排序 sort
+            @RequestParam(defaultValue = "create_date") String orderBy,
+            @RequestParam(defaultValue = "desc") String sort
             ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
         List<Product> productList= productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- src/main/java/com/kujudy/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/kujudy/springbootmall/dto/ProductQueryParams.java
@@ -2,9 +2,12 @@ package com.kujudy.springbootmall.dto;
 
 import com.kujudy.springbootmall.constant.ProductCategory;
 import lombok.Data;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Data
 public class ProductQueryParams {
     private ProductCategory category;
     private String search;
+    private String orderBy;
+    private String sort;
 }
